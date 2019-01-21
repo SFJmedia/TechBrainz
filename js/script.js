e7cd3f14aa37df19121d125de2ae27c946966e26
user <user@192.168.2.5>
2019-01-21 23:41:54 +0530

    about commit

--- js/script.js
+++ js/script.js
@@ -101,4 +101,45 @@ $(document).ready(function () {
         $('#back-to-top').tooltip('show');
     });
 // Scroll to top end
+// count set value start
+    $(function () {
+        function isScrolledIntoView($elem) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+            var elemTop = $elem.offset().top;
+            var elemBottom = elemTop + $elem.height();
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+
+        function count($this) {
+            var current = parseInt($this.html(), 10);
+            if (isScrolledIntoView($this) && !$this.data("isCounting") && current < $this.data('count')) {
+                $this.html(++current);
+                $this.data("isCounting", true);
+                setTimeout(function () {
+                    $this.data("isCounting", false);
+                    count($this);
+                }, 50);
+            }
+        }
+
+        $(".c-section4").each(function () {
+            $(this).data('count', parseInt($(this).html(), 10));
+            $(this).html('0');
+            $(this).data("isCounting", false);
+        });
+
+        function startCount() {
+            $(".c-section4").each(function () {
+                count($(this));
+            });
+        }
+        ;
+        $(window).scroll(function () {
+            startCount();
+        });
+        startCount();
+    });
+
+// count set value end
 });
